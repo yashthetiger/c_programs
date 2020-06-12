608e3bca644b4ee3bd4a12b38fa2fa165fa708bb
yashthetiger <64192060+yashthetiger@users.noreply.github.com>
2020-06-12 12:11:04 +0530

    upload sum_of_digits_recursion.c

---
+++ sum_of_digit_using_recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int sum_of_digits(int n);
+int main()
+{
+	int number;
+	printf("Input number: ");
+	scanf("%d",&number);
+	printf("Sum of digits: %d",sum_of_digits(number));
+	return 0;
+}
+
+int sum_of_digits(int n)
+{
+	int sum=0;
+	if(n!=0)
+		return sum+(n%10)+sum_of_digits(n/10);
+	else
+		return 0;
+}
