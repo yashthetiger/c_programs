09749c12439f46c5daeb05a70989617ad68a5128
yashthetiger <64192060+yashthetiger@users.noreply.github.com>
2020-06-12 12:14:20 +0530

    update sum_of_digits.c

--- sum_of_digit_using_recursion.c
+++ sum_of_digit_using_recursion.c
@@ -13,7 +13,7 @@ int sum_of_digits(int n)
 {
 	int sum=0;
 	if(n!=0)
-		return sum+(n%10)+sum_of_digits(n/10);
+		return sum+(n%10)+sum_of_digits(n/10);   //Statement Causing Recursion
 	else
 		return 0;
 }
