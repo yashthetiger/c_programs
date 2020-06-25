6a10db2ee8df9de9dcd49b75606a05debd463a55
yashthetiger <64192060+yashthetiger@users.noreply.github.com>
2020-06-25 12:39:31 +0530

    upload files.c
    printing the contents of a file in a reverse order
    and
    copying contents of a file to another

---
+++ files.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+int main()
+{
+	FILE *fp,*fp1;
+	char a[80];
+	char filename[20];
+	char filename1[20];   //file to which contents are to be copied
+	printf("Enter file name:");
+	scanf("%s",filename);
+	printf("Enter the copy filename:");
+	scanf("%s",filename1);
+	fp=fopen(filename,"r");
+	fp1=fopen(filename1,"w");
+	while(!feof(fp))
+	{
+		fgets(a,80,fp);
+		fputs(a,fp1);
+		printf("%s",strrev(a));
+	}
+	fclose(fp);
+	fclose(fp1);
+	return 0;
+}
